0e5b53ff67b5a143d3fb88a568da2ec22e8e26eb
JokisAtte <atte.jokinen@tuni.fi>
2020-07-08 16:05:29 +0300

    Front end done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { findAllByTestId } from '@testing-library/react';
+
 
 function App() {
+  const [weight, setNewWeight] = useState('')
+  const [fat, setFat] = useState('')
+  const [muscle, setMuscle] = useState('')
+
+  //Function that handles adding new data to the database
+  const addData = (event) => {
+    event.preventDefault()
+
+    var today = new Date()
+    var date = today.getDate() + '.' + today.getMonth() + '.' + today.getFullYear()
+
+    const dataObject = {
+      date: date,
+      weight: weight,
+      fat: fat,
+      muscle: muscle,
+    }
+    console.log(dataObject)
+  }
+
+  //Handlers for fields
+  const handleNewWeight = (event) => {
+    setNewWeight(event.target.value)
+  }
+
+  const handlefat = (event) => {
+    setFat(event.target.value)
+  }
+
+  const handleMuscle = (event) => {
+    setMuscle(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <h1>
+      Tervetuloa painonhallinta appiin. Syötä tiedot alle
+    </h1>
+
+    <div>
+      Paino: <input value = {weight} onChange = {handleNewWeight}/>
     </div>
+
+    <div>
+      Rasva-%: <input value = {fat} onChange = {handlefat} />
+    </div>
+
+    <div>
+      Lihas-% <input value = {muscle} onChange = {handleMuscle} />
+    </div>
+
+    <button onClick = {addData}>Tallenna</button>
+  </div>
+
   );
 }
 
